526a66df95d8972960a784ed975af03ec64bc23b
Anis <anis2k20@gmail.com>
2024-03-26 00:47:26 +0600

    Poppins font and heading tag size defined.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
+<!--  Main Container -->
   <div class="flex justify-center items-center h-screen">
-
+    <h1>Todo App</h1>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,18 @@ export default {
         'tuna':'#31363F',
         'moonstone-blue':'#76ABAE',
         'soft-peach':'#EEEEEE'
-
-      }
+      },
+      fontFamily : {
+        'poppins':['Poppins']
+      },
+      fontSize: {
+        'h1': '48px',
+        'h2': '36px',
+        'h3': '30px',
+        'h4': '24px',
+        'h5': '20px',
+        'h6': '16px',
+      },
     },
   },
   plugins: [],
