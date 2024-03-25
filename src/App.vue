ae1ef2e44093fe6796712e3a67bf746eab00abd1
Anis <anis2k20@gmail.com>
2024-03-26 00:02:57 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,vue}"],
+  theme: {
+    extend: {
+      colors:{
+        'dark':'#222831',
+        'tuna':'#31363F',
+        'moonstone-blue':'#76ABAE',
+        'soft-peach':'#EEEEEE'
+
+      }
+    },
+  },
+  plugins: [],
+}
+
